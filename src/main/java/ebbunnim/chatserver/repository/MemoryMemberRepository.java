1bba209044d30345a4920a4b338143cb77ed17ee
SJY <ebbunnim@gmail.com>
2021-01-06 23:02:25 +0900

    make repository + test login

---
+++ src/main/java/ebbunnim/chatserver/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package ebbunnim.chatserver.repository;
+
+import ebbunnim.chatserver.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    
+    // 실무에서는 동시성 문제때문에 hashMap, sequence 대신 다른 자료형 사용
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    // test 때 clear memory를 위한 코드
+    public void clearStore() {
+        store.clear();
+    }
+
+}
---
+++ src/test/java/ebbunnim/chatserver/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package ebbunnim.chatserver.repository;
+
+import ebbunnim.chatserver.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach // test가 끝날때마다 저장소를 clear함 (테스트가 서로 의존관계 없이 실행되도록) **
+    public void afterEach() {
+        repository.clearStore();
+    }
+    @Test
+    public void save() {
+        // save -> findById 해보기
+        Member member = new Member();
+        member.setName("ebbunnim");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get(); //Optional로 get으로 꺼내는데, test아니었으면 좋은 방법은 아니다.
+//        System.out.println("result"+(result.equals(member)));
+        Assertions.assertThat(member).isEqualTo(result); //검증
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("지영1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("지영2");
+        repository.save(member2);
+
+        Member result = repository.findByName("지영2").get();
+        Assertions.assertThat(result).isEqualTo(member2);
+    }
+
+    @Test
+    public void findByAll() {
+        Member member1 = new Member();
+        member1.setName("지영1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("지영2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        Assertions.assertThat(result.size()).isEqualTo(2);
+
+    }
+
+}
