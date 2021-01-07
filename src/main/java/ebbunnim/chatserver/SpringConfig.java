ea318014bdbb652b88412ec35f27265a16d9f7dc
SJY <ebbunnim@gmail.com>
2021-01-07 17:45:35 +0900

    jpa + integration test passed

--- src/main/java/ebbunnim/chatserver/SpringConfig.java
+++ src/main/java/ebbunnim/chatserver/SpringConfig.java
@@ -1,6 +1,7 @@
 package ebbunnim.chatserver;
 
 import ebbunnim.chatserver.repository.JdbcTemplateMemberRepository;
+import ebbunnim.chatserver.repository.JpaMemberRepository;
 import ebbunnim.chatserver.repository.MemberRepository;
 import ebbunnim.chatserver.repository.MemoryMemberRepository;
 import ebbunnim.chatserver.service.MemberService;
@@ -8,16 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -27,6 +29,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
